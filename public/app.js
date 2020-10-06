c869f2f81dca93619a30e8045737016328ce5ec1
Bosko <bosko.petreski@gmail.com>
2020-10-06 11:19:29 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -21,7 +21,7 @@ function getStyle(style) {
 function getFrontMenu() {
   $.get("/category", function(data, status) {
     var jsonData = JSON.parse(data);
-    $("#menu").html('<a style="font-size:18px;" href="/"><b>All News</b></a> |');
+    $("#menu").html('<a style="font-size:18px;" href="/"><b>Latest</b></a> |');
     $(jsonData).each(function(index, el) {
       if (el.id != 1) {
         $("#menu").append('&nbsp; <a style="font-size:18px;" href="'+(el.type == 2 ? '/list' : '/page')+ '/' + el.id + '"><b>' + el.title + "</b></a> |");
@@ -41,24 +41,18 @@ function getFrontPageStories() {
     renderFrontPageStories(jsonData);
   });
 }
-
-/////////
-// EDIT CONTENT
-////////
 function getFormatDate(timestamp) {
   var date = new Date(timestamp * 1000);
   var year = date.getFullYear();
   var month = "0" + (date.getMonth() + 1);
   var day = "0" + date.getDate();
-  var hours = date.getHours();
+  var hours = "0" + date.getHours();
   var minutes = "0" + date.getMinutes();
-  var formattedTime = year + "-" + month.substr(-2) + "-" + day.substr(-2);
-  //var formattedTime = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes.substr(-2);
+  var formattedTime = year + '-' + month.substr(-2) + '-' + day.substr(-2) + ' ' + hours.substr(-2) + ':' + minutes.substr(-2);
   return formattedTime;
 }
 function getTimestamp(dateString) {
-  //yyyy-MM-dd
-  var dateFormat = dateString.split("-");
-  var timestamp = new Date(dateFormat[0], dateFormat[1] - 1, dateFormat[2]);
-  return timestamp / 1000;
+  dateString = dateString.replace(" ", "T");
+  var timestamp = new Date(Date.parse(dateString+':00')).getTime() / 1000;
+  return timestamp;
 }
